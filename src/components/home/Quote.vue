401d13a74c60a026f62f3fcad065e0adca192b8a
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 16:39:33 +0200

    integrated quote section

---
+++ src/components/home/Quote.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <div class="fillblob flex flex-col items-center justify-center">
+        <p class="w-60 text-center text-darkblue text-2xl font-semibold">"inteligenta plus caracterul este scopul adevaratei educatii"</p>
+        <p class="text-white text-xl">- martin luther king jr</p>
+    </div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,11 +2,13 @@
 import Navbar from "../components/general/Navbar.vue";
 import Footer from "../components/general/Footer.vue";
 import Hero from "../components/home/Hero.vue";
+import Quote from "../components/home/Quote.vue";
 </script>
 <template>
   <body class="min-h-screen flex flex-col justify-between">
     <Navbar></Navbar>
     <Hero></Hero>
+    <Quote></Quote>
     <Footer></Footer>
   </body>
 </template>

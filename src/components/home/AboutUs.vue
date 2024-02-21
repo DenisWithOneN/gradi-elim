847ca211fd194d1727d190d4a786f5c45b3631f4
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 17:22:24 +0200

    about section integrated, content and design

---
+++ src/components/home/AboutUs.vue
@@ -0,0 +1,56 @@
+<script setup>
+const items = [
+  {
+    text: "Creşterea prestigiului grădiniţei prin aşezarea copilului şi a preocupărilor sale în centrul activităţii educaţionale.",
+  },
+  {
+    text: "Formarea şi întărirea motivaţiei preşcolarului pentru învăţarea şi informarea continuă.",
+  },
+  {
+    text: "Sprijinirea copiilor cu deficienţe.",
+  },
+  {
+    text: "Diversitatea prin accesibilitatea la educaţie în instituţiile noastre pentru toate grupurile etnice.",
+  },
+  {
+    text: "Asigurarea celor mai moderne condiţii pentru realizarea educaţiei grupurilor de copii defavorizaţi social.",
+  },
+  {
+    text: "Menţinerea, modernizarea şi optimizarea continuă a procesului instructiv-educativ, atât din punct de vedere a calităţii actului didactic cât şi a bazei tehnico-didactice.",
+  },
+];
+</script>
+<template>
+  <div class="aboutup spacer"></div>
+  <div class="bg-lightblue">
+    <div class="container mx-auto flex justify-around">
+      <!-- left image -->
+      <img
+        src="https://picsum.photos/2000"
+        alt="misiune&viziune"
+        class="rounded-3xl"
+        style="height: 30rem"
+      />
+
+      <!-- right content -->
+      <div class="w-1/2">
+        <h2 class="text-4xl text-yellow">misiune <br />& viziune</h2>
+        <p class="text-darkblue">
+          Grădiniţa noastră are o experienţă bogată în promovarea educaţiei la
+          vârstă fragedă , în formarea unor deprinderi intelectuale şi abilităţi
+          practice la copil, în funcţie de particularităţile de vârstă, la care
+          putem adăuga aspecte ale moralei creştine şi ale educaţiei
+          inter-culturale.
+        </p>
+        <!-- checkmarks -->
+        <div v-for="(item, index) in items" :key="index" class="flex">
+          <span class="text-yellow"><i class="bi bi-check-circle-fill"></i></span>
+          <p class="text-darkblue">{{ item.text }}</p>
+        </div>
+        <!-- /checkmarks -->
+      </div>
+      <!-- /right content -->
+    </div>
+  </div>
+  <div class="aboutdown spacer"></div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,12 +3,14 @@ import Navbar from "../components/general/Navbar.vue";
 import Footer from "../components/general/Footer.vue";
 import Hero from "../components/home/Hero.vue";
 import Quote from "../components/home/Quote.vue";
+import AboutUs from "../components/home/AboutUs.vue";
 </script>
 <template>
   <body class="min-h-screen flex flex-col justify-between">
     <Navbar></Navbar>
     <Hero></Hero>
     <Quote></Quote>
+    <AboutUs></AboutUs>
     <Footer></Footer>
   </body>
 </template>

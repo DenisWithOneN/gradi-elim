9ce9728f2436a4c4308df3dbdb5a4acf83e26c0c
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 21:34:45 +0200

    design update

--- src/components/general/Navbar.vue
+++ src/components/general/Navbar.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import FilledButton from "./FilledButton.vue";
 </script>
 <template>
   <nav class="spacer navbar">
     <div class="flex justify-between container mx-auto pt-2">
       <div class="flex items-center">
-      <img src="/src/assets/images/ELIM_LOGO.png" alt="logo" class="w-10 h-10">
-      <RouterLink to="/" class="mx-2">acasa</RouterLink>
-      <RouterLink to="/about" class="mx-2">despre noi</RouterLink>
-      <RouterLink to="/offer" class="mx-2">oferta educationala</RouterLink>
-      <RouterLink to="/gallery" class="mx-2">galerie</RouterLink>
-      <RouterLink to="/schedule" class="mx-2">orar</RouterLink>
+      <img src="/src/assets/images/ELIM_LOGO.png" alt="logo" class="w-10 h-10 mx-2">
+      <RouterLink to="/" class="uppercase font-semibold mx-2 text-darkblue">acasa</RouterLink>
+      <RouterLink to="/about" class="uppercase font-semibold mx-2 text-darkblue">despre noi</RouterLink>
+      <RouterLink to="/offer" class="uppercase font-semibold mx-2 text-darkblue">oferta educationala</RouterLink>
+      <RouterLink to="/gallery" class="uppercase font-semibold mx-2 text-darkblue">galerie</RouterLink>
+      <RouterLink to="/schedule" class="uppercase font-semibold mx-2 text-darkblue">orar</RouterLink>
     </div>
-    <RouterLink to="/contact" class=" flex items-center">contact</RouterLink>
+    <FilledButton color="bg-darkblue" text="contact" to="/contact"></FilledButton>
     </div>
   </nav>
 </template>

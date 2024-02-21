a6a7cadb06eb04e50d4d70b43b3eb9854541ee9f
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 15:11:49 +0200

    added bootstrap icons cdn, updated hero content

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,5 +1,28 @@
 <script setup></script>
 <template>
-    <p>test</p>
+  <div class="herowaves">
+    <!-- left side text -->
+    <div>
+      <p>gradinita elim hotar</p>
+      <h1>
+        bine ai venit <br />
+        gradi elim
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        facilis magnam! Maiores, corrupti sunt dolor molestias vero, quo ullam
+        facere explicabo, quaerat ut iure perspiciatis mollitia asperiores
+        nulla! Fuga, sed!
+      </p>
+      <!-- buttons -->
+      <div class="flex">
+        <button>inscrie-te</button>
+        <span
+          ><i class="bi bi-telephone-fill"></i>
+          <p>+40 745 572 157</p></span
+        >
+      </div>
+    </div>
+  </div>
 </template>
 <style></style>

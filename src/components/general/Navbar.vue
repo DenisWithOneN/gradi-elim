10a10ccbeff0800e7009dad82049d0b653b0a87f
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 14:08:52 +0200

    logo intergrated, navbar design updated

--- src/components/general/Navbar.vue
+++ src/components/general/Navbar.vue
@@ -2,15 +2,18 @@
 import { RouterLink } from "vue-router";
 </script>
 <template>
-  <nav class="container mx-auto flex justify-between">
-    <div>
+  <nav class="spacer navbar">
+    <div class="flex justify-between container mx-auto pt-2">
+      <div class="flex items-center">
+      <img src="/src/assets/ELIM_LOGO.png" alt="logo" class="w-10 h-10">
       <RouterLink to="/" class="mx-2">acasa</RouterLink>
       <RouterLink to="/about" class="mx-2">despre noi</RouterLink>
       <RouterLink to="/offer" class="mx-2">oferta educationala</RouterLink>
       <RouterLink to="/gallery" class="mx-2">galerie</RouterLink>
       <RouterLink to="/schedule" class="mx-2">orar</RouterLink>
     </div>
-    <RouterLink to="/contact">contact</RouterLink>
+    <RouterLink to="/contact" class=" flex items-center">contact</RouterLink>
+    </div>
   </nav>
 </template>
 <style></style>

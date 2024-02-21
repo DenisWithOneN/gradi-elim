3760ae1c3400ddfe96e67dfffa1863cfc4a016b0
DenisWithOneN <denis.ursut@emanuel.ro>
2024-02-21 14:48:06 +0200

    footer integrated, design and content update

---
+++ src/components/general/Footer.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+  <footer>
+    <div class="spacer footer"></div>
+    <div class="bg-yellow flex justify-around">
+      <span class="container mx-auto flex justify-around pb-10"
+        ><h2>gradinita elim hotar</h2>
+        <div>
+          <h3>despre noi</h3>
+          <ul>
+            <li>misiune</li>
+            <li>viziune</li>
+            <li>spatii educationale</li>
+            <li>modul de functionare</li>
+            <li>acreditare</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>oferta educationala</h3>
+          <ul>
+            <li>oferta curiculara</li>
+            <li>orar</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>contact</h3>
+          <ul>
+            <li>
+              gradinita cu program normal <br />
+              elim hotar
+            </li>
+            <li>Hotar, nr. 64, Bihor, ZIP 417606</li>
+            <li>elimhotar@yahoo.com</li>
+            <li>+40 745 572 157</li>
+          </ul>
+        </div></span
+      >
+    </div>
+  </footer>
+</template>
+<style></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Navbar from '../components/general/Navbar.vue';
-
-
+import Navbar from "../components/general/Navbar.vue";
+import Footer from "../components/general/Footer.vue";
 </script>
 <template>
+  <body class="min-h-screen flex flex-col justify-between">
     <Navbar></Navbar>
     <p>homepage</p>
+    <Footer></Footer>
+  </body>
 </template>
 <style></style>

83b79e45f8d28811c876f35c174efeb9abe7db26
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-02 20:45:18 +0200

    modified hero section for tablet view

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -2,17 +2,17 @@
 import FilledButton from "../general/FilledButton.vue";
 </script>
 <template>
-  <div class="container mx-auto flex justify-around items-center">
+  <div class="container mx-auto flex flex-col justify-around items-center lg:flex-row">
     <!-- left side content -->
-    <div class="w-1/2 flex flex-col">
-      <p class="text-sm uppercase font-body text-darkblue mb-2">
+    <div class=" w-full lg:w-1/2 flex flex-col order-last lg:order-first">
+      <p class="text-sm uppercase font-body text-darkblue mb-2 text-center lg:text-start mt-20 lg:mt-0">
         grădinița elim hotar
       </p>
-      <h1 class="text-7xl uppercase text-yellow font-heading font-black mb-3">
+      <h1 class="text-7xl uppercase  text-yellow font-heading font-black mb-3 text-center lg:text-start">
         bine ai venit <br />
         gradi elim
       </h1>
-      <p class="text-xl font-body text-darkblue mb-5">
+      <p class="text-xl font-body text-darkblue mb-5 text-center lg:text-start">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
         facilis magnam! Maiores, corrupti sunt dolor molestias vero, quo ullam
         facere explicabo, quaerat ut iure perspiciatis mollitia asperiores
@@ -20,24 +20,20 @@ import FilledButton from "../general/FilledButton.vue";
       </p>
 
       <!-- buttons -->
-      <div class="flex items-center">
-        <FilledButton
-          text="mai mult"
-          color="bg-lightblue"
-          to="/about"
-        ></FilledButton>
+      <div class="flex items-center justify-center lg:justify-normal mt-10 lg:mt-0">
+        <FilledButton text="mai mult" color="bg-lightblue" to="/about" class="order-last lg:order-first"></FilledButton>
         <!-- phone number -->
-        <span class="flex">
-          <span class="ml-2 flex items-center">
+        <div class="flex order-last lg:order-first">
+          <div class="ml-2 flex items-center">
             <!-- phone icon -->
-            <span class="flex bg-lightblue rounded-xl py-2 px-3"
-              ><i class="bi bi-telephone-fill text-white text-2xl"></i
-            ></span>
+            <div class="flex bg-lightblue rounded-xl py-2 px-3">
+              <i
+                class="bi bi-telephone-fill text-white text-2xl"></i></div>
             <p class="text-2xl font-semibold font-body text-lightblue ml-2">
               +40 745 572 157
-            </p></span
-          >
-        </span>
+            </p>
+          </div>
+        </div>
         <!-- /phone number -->
       </div>
       <!-- /buttons -->

7bdfabe1e4c520b87e21112e8cb794017acd85a7
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-04 16:04:32 +0200

    modified about us design

--- src/components/home/AboutUs.vue
+++ src/components/home/AboutUs.vue
@@ -2,60 +2,54 @@
 import FilledButton from "../general/FilledButton.vue";
 const items = [
   {
-    text: "Creşterea prestigiului grădiniţei prin aşezarea copilului şi a preocupărilor sale în centrul activităţii educaţionale.",
+    title: "Creşterea",
+    text: "prestigiului grădiniţei prin aşezarea copilului şi a preocupărilor sale în centrul activităţii educaţionale.",
   },
   {
-    text: "Formarea şi întărirea motivaţiei preşcolarului pentru învăţarea şi informarea continuă.",
+    title: "Formarea",
+    text: "şi întărirea motivaţiei preşcolarului pentru învăţarea şi informarea continuă.",
   },
   {
-    text: "Sprijinirea copiilor cu deficienţe.",
+    title: "Sprijinirea",
+    text: "copiilor cu deficienţe.",
   },
   {
-    text: "Diversitatea prin accesibilitatea la educaţie în instituţiile noastre pentru toate grupurile etnice.",
+    title: "Diversitatea",
+    text: "prin accesibilitatea la educaţie în instituţiile noastre pentru toate grupurile etnice.",
   },
   {
-    text: "Asigurarea celor mai moderne condiţii pentru realizarea educaţiei grupurilor de copii defavorizaţi social.",
+    title: "Asigurarea",
+    text: "celor mai moderne condiţii pentru realizarea educaţiei grupurilor de copii defavorizaţi social.",
   },
   {
-    text: "Menţinerea, modernizarea şi optimizarea continuă a procesului instructiv-educativ, atât din punct de vedere a calităţii actului didactic cât şi a bazei tehnico-didactice.",
+    title: "Menţinerea,",
+    text: "modernizarea şi optimizarea continuă a procesului instructiv-educativ, atât din punct de vedere a calităţii actului didactic cât şi a bazei tehnico-didactice.",
   },
 ];
 </script>
 <template>
   <div class="aboutup spacer"></div>
   <div class="bg-lightblue">
-    <div class="container mx-auto flex justify-around items-center">
-      <img src="https://picsum.photos/2000" alt="misiune&viziune" class="rounded-3xl mb-3 hidden lg:block"
-        style="height: 33rem" />
+    <div class="container mx-auto flex flex-col items-center">
+      <h2
+        class="text-5xl uppercase font-black font-heading text-center text-darkblue"
+      >
+        misiune & viziune
+      </h2>
+      <p class="text-xl font-body text-white text-center w-1/2">
+        Grădiniţa noastră are o experienţă bogată în educaţiei la vârstă
+        fragedă, în formarea unor deprinderi intelectuale şi abilităţi practice
+        la copil, în funcţie de particularităţile de vârstă, la care putem
+        adăuga aspecte ale moralei creştine şi ale educaţiei inter-culturale.
+      </p>
 
-      <!-- right content -->
-      <div class="w-full lg:w-1/2 pl-5 pb-3">
-        <h2 class="text-5xl uppercase font-black font-heading text-yellow hidden lg:block">
-          misiune <br />& viziune
-        </h2>
-        <h2 class="text-5xl uppercase font-black font-heading text-center text-yellow block lg:hidden">
-          misiune & viziune
-        </h2>
-        <p class="text-xl font-body text-white text-center lg:text-start">
-          Grădiniţa noastră are o experienţă bogată în educaţiei la vârstă
-          fragedă, în formarea unor deprinderi intelectuale şi abilităţi
-          practice la copil, în funcţie de particularităţile de vârstă, la care
-          putem adăuga aspecte ale moralei creştine şi ale educaţiei
-          inter-culturale.
-        </p>
-        <!-- checkmarks -->
-        <div v-for="(item, index) in items" :key="index" class="flex flex-col lg:flex-row my-5 items-center">
-          <div class="text-darkblue">
-            <i class="bi bi-check-circle-fill text-4xl"></i>
-          </div>
-          <p class="text-xl pl-2 font-body text-white text-center lg:text-start w-1/2 lg:w-full">{{ item.text }}</p>
+      <div class="flex flex-wrap justify-center mt-10">
+        <div v-for="(item, index) in items" :key="index" class="bg-yellow m-5 rounded-xl h-42 w-96 p-5">
+        <p class="text-3xl font-body text-darkblue">{{ item.title }}</p>
+        <p class="font-body text-lg text-darkblue">{{ item.text }}</p>
         </div>
-        <div class="flex justify-center lg:justify-start">
-          <FilledButton text="despre noi" color="bg-yellow" to="/about"></FilledButton>
-        </div>
-        <!-- /checkmarks -->
       </div>
-      <!-- /right content -->
+      <FilledButton text="despre noi" color="bg-darkblue" to="/about" class="mt-5"></FilledButton>
     </div>
   </div>
   <div class="aboutdown spacer"></div>

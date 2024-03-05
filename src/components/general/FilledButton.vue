318018059455945a6c611abdd518baccce048fbf
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-05 17:31:11 +0200

    adding dist

--- src/components/general/FilledButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { defineProps } from 'vue';
-import { RouterLink } from 'vue-router';
-const props = defineProps(['text', 'color', 'to']);
-
-</script>
-<template>
-    <RouterLink :class="props.color" class="p-2 px-3 rounded-xl text-2xl text-white font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
-</template>
--- src/components/home/Quote.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-<template>
-    
-        <div class="fillblob flex flex-col items-center justify-center"></div>
-  
-</template>
-<style scoped></style>

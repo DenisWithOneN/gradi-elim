158b53fdfaf2e50b7b3d5d250febea40baff0507
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-14 23:14:57 +0200

    created empty button

---
+++ src/components/general/EmptyButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+const props = defineProps(['text', 'color', 'to', 'size']);
+const borderColor = 'border-' + props.color
+const textColor = 'text-' + props.color
+
+</script>
+<template>
+    <RouterLink :class="borderColor, textColor, props.size" class="p-2 px-3 rounded-xl border-2 font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
+</template>

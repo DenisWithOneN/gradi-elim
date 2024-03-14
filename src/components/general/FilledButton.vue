6d9b73ea6566e0f1a989c83a69001088691eac56
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-14 23:15:58 +0200

    modified props value to be pure color, removing 'bg-' in props delcaration

--- src/components/general/FilledButton.vue
+++ src/components/general/FilledButton.vue
@@ -5,5 +5,5 @@ const props = defineProps(['text', 'color', 'to', 'size']);
 
 </script>
 <template>
-    <RouterLink :class="props.color, props.size" class="p-2 px-3 rounded-xl text-white font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
+    <RouterLink :class="'bg-'+props.color, props.size" class="p-2 px-3 rounded-xl text-white font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
 </template>

d7ed9c7e4abaa30a4d7dffcc121ef6eb008cd6f7
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-15 18:34:13 +0200

    bug: border color does not change

--- src/components/general/EmptyButton.vue
+++ src/components/general/EmptyButton.vue
@@ -2,10 +2,9 @@
 import { defineProps } from 'vue';
 import { RouterLink } from 'vue-router';
 const props = defineProps(['text', 'color', 'to', 'size']);
-const borderColor = 'border-' + props.color
-const textColor = 'text-' + props.color
+
 
 </script>
 <template>
-    <RouterLink :class="borderColor, textColor, props.size" class="p-2 px-3 rounded-xl border-2 font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
+    <RouterLink :class="'border-'+props.color, 'text-'+props.color, props.size" class="p-2 px-3 rounded-xl border-4 font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
 </template>

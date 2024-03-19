bef8557c47d801ccffb96238e1e634db2be28926
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-19 13:48:56 +0200

    design changes, responsivness

--- src/components/home/AboutUs.vue
+++ src/components/home/AboutUs.vue
@@ -33,20 +33,20 @@ const items = [
   <div class="bg-lightblue">
     <div class="container mx-auto flex flex-col items-center">
       <h2
-        class="text-darkblue text-4xl text-center font-black font-heading uppercase sm:text-5xl md:text-6xl lg:text-7xl">
+        class="text-darkblue text-5xl text-center font-black font-heading uppercase md:text-6xl lg:text-7xl">
         misiune & viziune
       </h2>
-      <p class="text-white text-sm text-center font-body max-w-sm mx-5 md:text-xl lg:max-w-4xl">
+      <p class="text-white text-sm sm:text-lg md:text-xl xl:text-2xl text-center font-body max-w-sm md:max-w-lg mx-5 lg:max-w-4xl">
         Grădiniţa noastră are o experienţă bogată în educaţiei la vârstă
         fragedă, în formarea unor deprinderi intelectuale şi abilităţi practice
         la copil, în funcţie de particularităţile de vârstă, la care putem
         adăuga aspecte ale moralei creştine şi ale educaţiei inter-culturale.
       </p>
 
-      <div class="flex flex-wrap justify-center mt-10">
-        <div v-for="(item, index) in items" :key="index" class="bg-yellow rounded-2xl drop-shadow-aboutCard w-72 h-60 m-5 p-5">
-          <p class="text-darkblue text-3xl font-body">{{ item.title }}</p>
-          <p class="text-darkblue text-lg font-body">{{ item.text }}</p>
+      <div class="flex flex-wrap justify-center lg:mt-5">
+        <div v-for="(item, index) in items" :key="index" class="bg-yellow rounded-2xl drop-shadow-aboutCard w-60 h-44 md:w-72 md:h-80 m-5 p-5">
+          <p class="text-darkblue text-3xl md:text-4xl font-body">{{ item.title }}</p>
+          <p class="text-darkblue text-sm  md:text-xl xl:text-2xl font-body">{{ item.text }}</p>
         </div>
       </div>
       <FilledButton :text="`despre noi`" :bgColor="`bg-darkblue`" :size="`text-xl xl:text-2xl`" :to="`/about`" class="mt-10"></FilledButton>

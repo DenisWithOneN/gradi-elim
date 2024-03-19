c95d65611f759cb2d6f50581261bf2b09b105d5b
DenisWithOneN <denis.ursut@emanuel.com>
2024-03-19 09:08:33 +0200

    changed props from empty button

--- src/components/general/EmptyButton.vue
+++ src/components/general/EmptyButton.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { defineProps } from 'vue';
 import { RouterLink } from 'vue-router';
-const props = defineProps(['text', 'color', 'to', 'size']);
+
+const props = defineProps({
+    text: String,
+    color: String,
+    backgroundColor: String,
+    to: String,
+    size: String,
+});
+
+
 
 
 </script>
 <template>
-    <RouterLink :class="'border-'+props.color, 'text-'+props.color, props.size" class="p-2 px-3 rounded-xl border-4 font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
+    <RouterLink :class="`border-${props.color}`, `text-${props.color}`, props.backgroundColor, props.size" class="p-2 px-3 rounded-xl border-2 font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
 </template>
--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -4,32 +4,34 @@ import EmptyButton from "../general/EmptyButton.vue"
 </script>
 
 <template>
-  <div class="container mx-auto flex flex-col justify-around items-center lg:flex-row">
+  <div class="container mx-auto px-10 flex flex-col justify-center items-center lg:flex-row">
     <!-- left side content -->
     <div class="w-full lg:w-1/2 flex flex-col order-last lg:order-first">
       <div class="flex flex-col items-center lg:items-start">
         <p class="text-sm uppercase font-body text-darkblue mb-2 text-center lg:text-start mt-5 lg:mt-0">
           grădinița elim hotar
         </p>
-        <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl uppercase  text-yellow font-heading font-black mb-3 text-center lg:text-start">
+        <h1
+          class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl uppercase  text-yellow font-heading font-black mb-3 text-center lg:text-start">
           bine ai venit <br />
           gradi elim
         </h1>
         <p class="text-sm md:text-xl mx-10 lg:mx-0 font-body text-darkblue mb-5 text-center lg:text-start">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-          facilis magnam! Maiores, corrupti sunt dolor molestias vero, quo ullam
-          facere explicabo, quaerat ut iure perspiciatis mollitia asperiores
-          nulla! Fuga, sed!
+          Explorăm, învățăm și creștem împreună într-o oază plină de bucurie și iubire, chiar în inima comunității
+          noastre rurale. La Grădinița Elim, fiecare zi este o aventură plină de descoperiri, iar micuții noștri sunt
+          ghidați cu grijă și afecțiune pe drumul cunoașterii.
         </p>
       </div>
 
       <!-- buttons -->
       <div class="flex flex-col sm:flex-row items-center justify-center lg:justify-normal">
-        <FilledButton size="text-2xl" text="mai mult" color="lightblue" to="/about" class="order-last lg:order-first"></FilledButton>
+        <FilledButton size="text-2xl" text="mai mult" color="lightblue" to="/about" class="order-last lg:order-first">
+        </FilledButton>
         <!-- phone number -->
-        <div class="flex order-last lg:order-first">
+        <div class="flex order-last lg:order-first">  
           <div class="ml-2 flex items-center">
-            <EmptyButton size="text-2xl" text="contact" color="lightblue" to="/contact" class="order-last lg:order-first"></EmptyButton>
+            <EmptyButton :size="'text-2xl'" :text="'contact'" :color="'lightblue'" :backgroundColor="'bg-white'" :to="'/contact'"
+              class="order-last lg:order-first"></EmptyButton>
           </div>
         </div>
         <!-- /phone number -->

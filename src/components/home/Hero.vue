db360169c33f448bfcc7c5a9619db656c36a506a
DenisWithOneN <denis.ursut@emanuel.com>
2024-03-19 11:35:56 +0200

    removed temporary border

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -8,22 +8,23 @@ import EmptyButton from "../general/EmptyButton.vue"
     <!-- left side content -->
     <div class="flex flex-col order-last lg:order-first">
       <div class="flex flex-col items-center lg:items-start">
-        <p class="border-2 text-sm uppercase font-body text-darkblue mb-2 text-center lg:text-start mt-5 lg:mt-0">
+        <p class=" text-sm uppercase font-body text-darkblue mb-2 text-center lg:text-start mt-5 lg:mt-0">
           grădinița elim hotar
         </p>
         <h1
-          class="border-2 text-mobileHeading uppercase text-yellow font-heading font-black text-center mb-3 sm:text-5xl md:text-6xl lg:text-start lg:text-7xl">
-          bine ai venit 
+          class="text-mobileHeading uppercase text-yellow font-heading font-black text-center mb-3 sm:text-5xl md:text-6xl lg:text-start lg:text-7xl">
+          bine ai venit <br>
           gradi elim
         </h1>
-        <p class="border-2 text-darkblue text-sm max-w-lg font-body text-center mb-5 sm:text-lg md:text-xl lg:text-start lg:mx-0">
-          Explorăm, învățăm și creștem împreună într-o oază plină de bucurie și iubire, chiar în inima comunității 
+        <p
+          class="text-darkblue text-sm max-w-lg font-body text-center mb-5 sm:text-lg md:text-xl lg:text-start lg:mx-0">
+          Explorăm, învățăm și creștem împreună într-o oază plină de bucurie și iubire, chiar în inima comunității
           noastre rurale. La Grădinița Elim, fiecare zi este o aventură plină de descoperiri, iar micuții noștri sunt
           ghidați cu grijă și afecțiune pe drumul cunoașterii.
         </p>
       </div>
       <!-- buttons -->
-      <div class="border-2 flex flex-col items-center justify-center sm:flex-row lg:justify-normal">
+      <div class="flex flex-col items-center justify-center sm:flex-row lg:justify-normal">
         <FilledButton size="text-xl" text="mai mult" color="lightblue" to="/about" class="my-2 sm:my-0 sm:mr-2">
         </FilledButton>
         <EmptyButton :size="'text-xl'" :text="'contact'" :color="'lightblue'" :backgroundColor="'bg-white'"
@@ -32,7 +33,7 @@ import EmptyButton from "../general/EmptyButton.vue"
       <!-- /buttons -->
     </div>
     <!-- right side image -->
-  <img src="/src/assets/images/herophoto.png" alt="hero image" class="h-52 sm:h-96 xl:h-xlCustom border-2"/>
+    <img src="/src/assets/images/herophoto.png" alt="hero image" class="h-52 sm:h-96 xl:h-xlCustom" />
 
   </div>
 

d557ff5ba927808b4a5d0a65a6e5e94b8af4d020
DenisWithOneN <denis.ursut@emanuel.com>
2024-03-19 09:50:52 +0200

    added custom font size for mobile headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,11 @@ module.exports = {
       },
       dropShadow: {
         'custom': '0px 4px 2px rgba(0, 0, 0, 0.25)',
-      }
+      },
+      fontSize: {
+        mobileHeading: ['2.5rem', '2.5rem']
+      
+      },
     },
   },
   variants: {

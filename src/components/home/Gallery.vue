514d6b67e25bc033814968b9fc1eed19b1088f44
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-21 14:31:15 +0200

    first test of background image as ref

--- src/components/home/Gallery.vue
+++ src/components/home/Gallery.vue
@@ -1,14 +1,22 @@
 <script setup>
-import FilledButton from "../general/FilledButton.vue";
+import {ref} from 'vue'
+const photos = ref([
+  {
+    imageURL: "/src/assets/gallery-images/ZVE05696.jpg",
+  },
+])
+
 </script>
 <template>
   <div class="galleryup spacer"></div>
   <div class="bg-green pb-5">
     <div class="container mx-auto flex flex-col justify-center items-center">
-      <h2 class="text-darkblue text-5xl uppercase text-center font-black font-heading mb-16 md:text-6xl lg:text-7xl">
+      <h2 class="text-darkblue text-5xl uppercase text-center font-black font-heading mb-5 md:text-6xl lg:text-7xl">
         galerie
       </h2>
-      <div class="flex gap-4">
+
+      <!-- desktop gallery -->
+      <div class="hidden lg:flex gap-4">
         <div class="photo1 rounded-2xl"></div>
         <div class="flex flex-col gap-4">
           <div class="photo2 h-48 rounded-2xl"></div>
@@ -25,12 +33,21 @@ import FilledButton from "../general/FilledButton.vue";
         </div>
       </div>
 
+      <!-- tablet/phone gallery -->
+      <div v-for="(photo, index) in photos" :key="index" class="flex lg:hidden">
+        <div class="h-96 w-96 photoProps" :style="{backgroundImage: `url(${photo.imageURL})`}"></div>
+      </div>
 
     </div>
   </div>
   <div class="gallerydown spacer"></div>
 </template>
 <style scoped>
+.photoProps{
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 .photo1 {
   width: 25rem;
   height: 25rem;

59c22af4eb46fe0ca3d2f76980216c278a1ba420
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-21 14:17:53 +0200

    prettier format for tailwindcss - failed?

--- src/components/general/FilledButton.vue
+++ src/components/general/FilledButton.vue
@@ -10,9 +10,11 @@ const props = defineProps({
     size: String,
 })
 
-console.log("culoare "+props.bgColor)
+console.log("culoare " + props.bgColor)
 </script>
 
 <template>
-    <RouterLink :class="props.bgColor, props.size" class="drop-shadow-custom p-2 px-3 rounded-xl text-white font-semibold font-body uppercase" :to="to">{{ props.text }}</RouterLink>
+    <RouterLink :class="props.bgColor, props.size"
+        class="drop-shadow-custom p-2 px-3 rounded-xl text-white font-semibold font-body uppercase" :to="to">{{
+        props.text }}</RouterLink>
 </template>

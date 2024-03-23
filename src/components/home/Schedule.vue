1206fde624ebf0a85dc5931acf63637e7a1d5205
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-23 22:12:13 +0200

    finished design, non responsive

--- src/components/home/Schedule.vue
+++ src/components/home/Schedule.vue
@@ -6,14 +6,16 @@ const hours = [
     secondHour: "09:00",
     activity: "primirea copiilor",
     dropshadowColor: "drop-shadow-scheduleCardGreen",
-    image: "src/assets/images/Layer 1.png"
+    image: "src/assets/images/Layer 1.png",
+    color: "text-green"
   },
   {
     firstHour: "09:00",
     secondHour: "10:00",
     activity: "activitati pe domenii experientiale",
     dropshadowColor: "drop-shadow-scheduleCardBlue",
-    image: "src/assets/images/Layer 2.png"
+    image: "src/assets/images/Layer 2.png",
+    color: "text-lightblue"
   },
 
   {
@@ -21,37 +23,46 @@ const hours = [
     secondHour: "10:45",
     activity: "gustarea",
     dropshadowColor: "drop-shadow-scheduleCardYellow",
-    image: "src/assets/images/Layer 3.png"
+    image: "src/assets/images/Layer 3.png",
+    color: "text-yellow"
   },
   {
     firstHour: "10:45",
     secondHour: "11:30",
     activity: "jocuri si activitati didactice alese",
     dropshadowColor: "drop-shadow-scheduleCardGreen",
-    image: "src/assets/images/Layer 4.png"
+    image: "src/assets/images/Layer 4.png",
+    color: "text-green"
   },
   {
     firstHour: "11:30",
     secondHour: "12:30",
     activity: "activitati pe domenii experientiale",
     dropshadowColor: "drop-shadow-scheduleCardBlue",
-    image: "src/assets/images/Layer 5.png"
+    image: "src/assets/images/Layer 5.png",
+    color: "text-lightblue"
   },
   {
     firstHour: "12:30",
     secondHour: "13:00",
     activity: "jocuri recreative",
     dropshadowColor: "drop-shadow-scheduleCardYellow",
-    image: "src/assets/images/Layer 6.png"
+    image: "src/assets/images/Layer 6.png",
+    color: "text-yellow"
   },
 ];
 </script>
 <template>
-
-  <div v-for="(hour, index) in hours" :key="index">
-    <div class="bg-white">
-      <img :src="hour.image" alt="gallery photo" class="h-14" :class="`${hour.dropshadowColor}`">
+  <section class="flex flex-col items-center justify-center gap-y-10">
+  <h2 class="text-yellow text-5xl md:text-6xl lg:text-7xl font-heading uppercase font-black">orar</h2>
+  <div class="grid grid-cols-3 place-items-center  gap-10  mx-auto">
+    <div v-for="(hour, index) in hours" :key="index" class=" bg-white flex flex-col items-center pt-5 p-2 w-52 h-52 rounded-2xl" :class="hour.dropshadowColor">
+      <img :src="hour.image" alt="gallery photo" class="h-14">
+      <p class="text-xl md:text-2xl lg:text-3xl font-bold font-heading" :class="`${hour.color}`">{{ hour.firstHour }} - {{ hour.secondHour }}</p>
+      <p class="text-darkblue font-body text-sm sm:text-lg md:text-xl xl:text-2xl text-center">{{ hour.activity }}</p>
     </div>
   </div>
-  
+  <FilledButton :bgColor='`bg-yellow`' :text="`orar complet`" :size="`text-xl`" :to="`/schedule`"></FilledButton>
+</section>
+
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,9 @@ module.exports = {
       dropShadow: {
         'custom': '0px 4px 2px rgba(0, 0, 0, 0.25)',
         'aboutCard': '13px 15px rgba(69, 86, 99, 1)',
-        'scheduleCardGreen': '13px 15px rgba(177, 195, 129, 1)',
-        'scheduleCardBlue': '13px 15px rgba(155, 184, 205, 1)',
-        'scheduleCardYellow': '13px 15px rgba(238, 199, 89, 1)',
+        'scheduleCardGreen': '0px 0px 5px rgba(177, 195, 129, 1)',
+        'scheduleCardBlue': '0px 0px 5px rgba(155, 184, 205, 1)',
+        'scheduleCardYellow': '0px 0px 5px rgba(238, 199, 89, 1)',
       },
       fontSize: {
         mobileHeading: ['2.5rem', '2.5rem']

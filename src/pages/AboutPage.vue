7a29400aeebe93190991cfd6c1dded85abc11a86
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-29 20:04:40 +0200

    integrated first part of the section, responsive

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,15 +1,75 @@
 <script setup>
 import Navbar from "../components/general/Navbar.vue";
 import Footer from "../components/general/Footer.vue";
+import FilledButton from "../components/general/FilledButton.vue";
 </script>
 <template>
-
   <body class="min-h-screen flex flex-col justify-between">
     <Navbar></Navbar>
-    <div class="container mx-auto flex justify-center ">
-      <p class="text-5xl font-heading text-darkblue uppercase">pagina in lucru...</p>
-    </div>
+    <section class="container mx-auto">
+      <div
+        class="flex flex-col lg:flex-row justify-center items-center gap-5 mx-10"
+      >
+        <div
+          class="photo1 rounded-2xl w-52 h-52 md:w-60 md:h-60 lg:w-96 lg:h-96 lg:order-last"
+        ></div>
+        <div class="flex flex-col items-center lg:items-start gap-3">
+          <h1
+            class="text-center font-heading font-black uppercase text-green text-5xl md:text-6xl lg:text-7xl lg:text-start"
+          >
+            despre noi
+          </h1>
+          <div class="text-center lg:text-start">line</div>
+          <p
+            class="text-darkblue text-center font-body text-sm sm:text-lg md:text-xl xl:text-2xl lg:max-w-lg lg:text-start"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+            incidunt molestias? Repellat corporis eos optio non illo distinctio
+            minus, aliquam odit praesentium, provident aliquid ad cum molestias
+            quae. Quis, dolores?
+          </p>
+          <FilledButton
+            :text="`contact`"
+            :bgColor="`bg-green`"
+            :to="`/contact`"
+            :size="`text-xl xl:text-2xl`"
+          ></FilledButton>
+        </div>
+      </div>
+    </section>
     <Footer></Footer>
   </body>
 </template>
-<style></style>
+<style scoped>
+.photo1 {
+  background-image: url("src/assets/gallery-images/ZVE05452.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.photo2 {
+  background-image: url("src/assets/gallery-images/ZVE05452.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.photo3 {
+  background-image: url("src/assets/gallery-images/ZVE05452.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.photo4 {
+  background-image: url("src/assets/gallery-images/ZVE05452.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.photo5 {
+  background-image: url("src/assets/gallery-images/ZVE05452.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>

d5a3e0f136c97d2ee0acdb6f6f7728fdaf8fece7
DenisWithOneN <denis.ursut@emanuel.ro>
2024-03-29 19:45:20 +0200

    temporary setting color blue to all buttons till later props fix

--- src/components/general/FilledButton.vue
+++ src/components/general/FilledButton.vue
@@ -1,20 +1,22 @@
 <script setup>
-import { defineProps } from 'vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 
 //defined as String for TailwindCSS
 const props = defineProps({
-    text: String,
-    bgColor: String,
-    to: String,
-    size: String,
-})
+  text: String,
+  bgColor: String,
+  to: String,
+  size: String,
+});
 
-console.log("culoare " + props.bgColor)
+console.log("culoare " + props.bgColor);
 </script>
 
 <template>
-    <RouterLink :class="props.bgColor, props.size"
-        class="drop-shadow-custom p-2 px-3 rounded-xl text-white font-semibold font-body uppercase" :to="to">{{
-        props.text }}</RouterLink>
+  <RouterLink
+    :class="(props.bgColor, props.size)"
+    class="drop-shadow-custom p-2 px-3 rounded-xl bg-blue-500 text-white font-semibold font-body uppercase"
+    :to="to"
+    >{{ props.text }}</RouterLink
+  >
 </template>
